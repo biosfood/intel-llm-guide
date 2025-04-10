4cb89531967403d01ab33d8c8b93bb3523729ad2
biosfood <leisenhau@gmail.com>
2025-04-10 08:40:57 +0200

    update

--- example.py
+++ example.py
@@ -3,9 +3,7 @@ from intel_extension_for_transformers.transformers import AutoModelForCausalLM
 
 prompt = "Once upon a time, there existed a little girl,"
 
-model_name="/opt/models/TinyLlama/TinyLlama-1.1B-Chat-v1.0/"
-
-model_name="/home/lukas/.cache/huggingface/hub/models--Intel--neural-chat-7b-v3-3/snapshots/7b86016aa1d2107440c1928694a7bba926509887/"
+model_name="/opt/models/microsoft/phi-2/"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name, local_files_only=True)
 inputs = tokenizer(prompt, return_tensors="pt").input_ids
